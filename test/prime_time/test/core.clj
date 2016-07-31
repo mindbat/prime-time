a716e910dbeefd547370e31c09a644b311b91a75
Ron Toland <rontoland@gmail.com>
2016-07-31 14:01:11 -0500

    Initial tests for prime generation

--- test/prime_time/test/core.clj
+++ test/prime_time/test/core.clj
@@ -2,6 +2,15 @@
   (:require [clojure.test :refer :all]
             [prime-time.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest t-generate-primes
+  ;; generate the first 3 primes
+  (let [expected [1 2 3]]
+    (is (= expected
+           (generate-primes 3))))
+  ;; generate the first 10 primes
+  (let [expected [1 2 3 5 7 11 13 17 19 23]]
+    (is (= expected
+           (generate-primes 10))))
+  ;; generate the first 100 primes without oome
+  (is (= 100
+         (count (generate-primes 100)))))
