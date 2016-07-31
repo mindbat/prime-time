ab01476c895d97df0f30610904021f4f32aa36f1
Ron Toland <rontoland@gmail.com>
2016-07-31 14:01:26 -0500

    Initial functions for generating primes

--- src/prime_time/core.clj
+++ src/prime_time/core.clj
@@ -1,6 +1,27 @@
 (ns prime-time.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn factor?
+  "Returns true if n is a factor of x"
+  [x n]
+  (= 0 (mod x n)))
+
+(defn prime?
+  "Checks x against a given list of primes. Returns true if x is also prime"
+  [primes x]
+  (not (boolean (seq (filter (partial factor? x) primes)))))
+
+(defn next-prime
+  "Given a number, return the next prime after it"
+  [primes current]
+  (first (filter (partial prime? primes)
+                 (iterate inc current))))
+
+(defn generate-primes
+  "Return a list of the first num-primes primes"
+  [num-primes]
+  (loop [primes [2]
+         next 2]
+    (if (= (- num-primes 1) (count primes))
+      (apply conj [1] primes)
+      (recur (conj primes (next-prime primes next))
+             (inc next)))))
